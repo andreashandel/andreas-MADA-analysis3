13e972cdace1503d1e34be577210d5e63d0aa7bf
Andreas Handel <andreas.handel@gmail.com>
2021-10-06 20:08:42 -0400

    start

--- code/processing_code/processingscript.R
+++ code/processing_code/processingscript.R
@@ -7,11 +7,12 @@
 #load needed packages. make sure they are installed.
 library(readxl) #for loading Excel files
 library(dplyr) #for data processing
+library(tidyr) #for data processing
 library(here) #to set paths
 
 #path to data
 #note the use of the here() package and not absolute paths
-data_location <- here::here("data","raw_data","exampledata.xlsx")
+data_location <- here::here("data","raw_data","SympAct_Any_Pos.Rda")
 
 #load data. 
 #note that for functions that come from specific packages (instead of base R)
@@ -19,47 +20,25 @@ data_location <- here::here("data","raw_data","exampledata.xlsx")
 #package::function() that's not required one could just call the function
 #specifying the package makes it clearer where the function "lives",
 #but it adds typing. You can do it either way.
-rawdata <- readxl::read_excel(data_location)
+rawdata <- readRDS(data_location)
 
-#take a look at the data
-dplyr::glimpse(rawdata)
+#take a look at variables of the data frame
+colnames(rawdata)
 
-#dataset is so small, we can print it to the screen.
-#that is often not possible.
-print(rawdata)
+#remove those variables we don't want
+#note that in this data frame, the ones we don't want are at the beginning and end, so we could have also removed
+#by location. But that's not as robust, if you get an update on the data and someone reorganized the data, your code will
+#produce the wrong results. And you might not even know it. Removal by name is generally safer.
+#If someone renames and the column doesn't exist, you should get an error message, which is much better than code that still works
+#but does the wrong thing.
+d1 <- rawdata %>% dplyr::select(-contains(c("Score","Total","FluA","FluB","Dxname","Activity","Unique.Visit")))
 
-# looks like we have measurements for height (in centimeters) and weight (in kilogram)
 
-# there are some problems with the data: 
-# There is an entry which says "sixty" instead of a number. 
-# Does that mean it should be a numeric 60? It somehow doesn't make
-# sense since the weight is 60kg, which can't happen for a 60cm person (a baby)
-# Since we don't know how to fix this, we need to remove the person.
-# This "sixty" entry also turned all Height entries into characters instead of numeric.
-# We need to fix that too.
-# Then there is one person with a height of 6. 
-# that could be a typo, or someone mistakenly entered their height in feet.
-# Since we unfortunately don't know, we'll have to remove this person.
-# similarly, there is a person with weight of 7000, which is impossible,
-# and one person with missing weight.
-# to be able to analyze the data, we'll remove those 5 individuals
-
-# this is one way of doing it. Note that if the data gets updated, 
-# we need to decide if the thresholds are ok (newborns could be <50)
-
-processeddata <- rawdata %>% dplyr::filter( Height != "sixty" ) %>% 
-                             dplyr::mutate(Height = as.numeric(Height)) %>% 
-                             dplyr::filter(Height > 50 & Weight < 1000)
-
-# save data as RDS
-# I suggest you save your processed and cleaned data as RDS or RDA/Rdata files. 
-# This preserves coding like factors, characters, numeric, etc. 
-# If you save as CSV, that information would get lost.
-# See here for some suggestions on how to store your processed data:
-# http://www.sthda.com/english/wiki/saving-data-into-r-data-format-rds-and-rdata
+#remove NA
+d2 <- d1 %>% tidyr::drop_na()
 
 # location to save file
-save_data_location <- here::here("data","processed_data","processeddata.rds")
+save_data_location <- here::here("d2","processed_data","processeddata.rds")
 
 saveRDS(processeddata, file = save_data_location)
 
